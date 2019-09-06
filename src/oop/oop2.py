54c781ec82e0149f7c874817917d038a2d36dce5
joepound <14357797+joepound@users.noreply.github.com>
2019-09-06 09:24:38 -0600

    Completed GroundVehicle class for oop2 challenge

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -1,22 +1,27 @@
-# To the GroundVehicle class, add method drive() that returns "vroooom".
-#
-# Also change it so the num_wheels defaults to 4 if not specified when the
-# object is constructed.
+"""
+To the GroundVehicle class, add method drive() that returns "vroooom".
+
+Also change it so the num_wheels defaults to 4 if not specified when the
+object is constructed.
+"""
 
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
-    # TODO
+    def drive(self):
+        return "vroooom"
 
 
-# Subclass Motorcycle from GroundVehicle.
-#
-# Make it so when you instantiate a Motorcycle, it automatically sets the
-# number of wheels to 2 by passing that to the constructor of its superclass.
-#
-# Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+"""
+Subclass Motorcycle from GroundVehicle.
+
+Make it so when you instantiate a Motorcycle, it automatically sets the
+number of wheels to 2 by passing that to the constructor of its superclass.
+
+Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+"""
 
 # TODO
 
