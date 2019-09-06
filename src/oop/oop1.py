9fecbdc7f95bc535f3f5bc60d9a6ae048a5e1494
joepound <14357797+joepound@users.noreply.github.com>
2019-09-06 09:20:58 -0600

    Added classes for oop1 challenge
    All tests passed.

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -1,19 +1,70 @@
-# Write classes for the following class hierarchy:
-#
-#  [Vehicle]->[FlightVehicle]->[Starship]
-#      |                |
-#      v                v
-# [GroundVehicle]      [Airplane]
-#   |       |
-#   v       v
-# [Car]  [Motorcycle]
-#
-# Each class can simply "pass" for its body. The exercise is about setting up
-# the hierarchy.
-#
-# e.g.
-#
-# class Whatever:
-#     pass
-#
-# Put a comment noting which class is the base class
+"""
+Write classes for the following class hierarchy:
+
+ [Vehicle]->[FlightVehicle]->[Starship]
+     |                |
+     v                v
+[GroundVehicle]      [Airplane]
+  |       |
+  v       v
+[Car]  [Motorcycle]
+
+Each class can simply "pass" for its body. The exercise is about setting up
+the hierarchy.
+
+e.g.
+
+class Whatever:
+    pass
+
+Put a comment noting which class is the base class
+"""
+
+# Base class
+
+
+class Vehicle:
+    def __init__(self):
+        pass
+
+
+# Ground vehicles
+
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        pass
+
+
+class Car(GroundVehicle):
+    def __init__(self):
+        pass
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        pass
+
+
+# Flight vehicles
+
+
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        pass
+
+
+# Aircraft
+
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        pass
+
+
+# Spacecraft
+
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        pass
